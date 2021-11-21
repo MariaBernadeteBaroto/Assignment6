2b7378705566f17b560fa420b27509bf87096786
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-11-21 02:04:14 -0800

    finished condition2

--- programfile1.py
+++ programfile1.py
@@ -14,8 +14,14 @@ else:
     low1 = number2
     high1 = number2    # false block
 
-
 # condtion 2
+if number3 < number4:
+    low2 = number3
+    high2 = number4    # true block
+else:
+    low2 = number4
+    high2 = number3     # false block
+
 # condition 3
 # condition 4
 # condition 5
