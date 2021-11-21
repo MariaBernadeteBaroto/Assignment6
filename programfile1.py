8b43a4412f9d9c13c0adb194b473269298cc7622
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-11-21 01:57:16 -0800

    finished input and first condition

---
+++ programfile1.py
@@ -0,0 +1,21 @@
+
+# 1. Ask for four numbers
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+number3 = int(input("Enter third number: "))
+number4 = int(input("Enter fourth number: "))
+
+# condtion 1
+if number1 < number2:
+    low1 = number1
+    high1 = number2    # true block
+else: 
+    low1 = number2
+    high1 = number2    # false block
+
+
+# condtion 2
+# condition 3
+# condition 4
+# condition 5
